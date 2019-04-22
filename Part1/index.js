d8e18b9dbbf19ab308b54fc3d1206d2b0b572991
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-22 15:10:55 -0600

    fifth

--- Part1/index.js
+++ Part1/index.js
@@ -13,3 +13,13 @@ function play(clickedId) {
       }
     }
     console.log("board")
+
+    const topLeft = board[0];
+  const topCenter = board[1];
+  const topRight = board[2];
+  const middleLeft = board[3];
+  const middleCenter = board[4];
+  const middleRight = board[5];
+  const bottomLeft = board[6];
+  const bottomCenter = board[7];
+  const bottomRight = board[8];
