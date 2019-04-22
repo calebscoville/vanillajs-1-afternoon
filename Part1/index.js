32d5f09e6f679de88d84f01ae0e0704d3358e2f3
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-22 14:41:13 -0600

    Second

--- Part1/index.js
+++ Part1/index.js
@@ -1,5 +1,15 @@
-console.log("ran")
 
-function play(player) {
-document.getElementsById('player')[0].className = theme
-}
+
+function play(clickedId) {
+    const playerSpan = document.getElementById('player');
+    const clickedElement = document.getElementById(clickedId);
+
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O';
+        clickedElement.innerText = 'X';
+      } else {
+        playerSpan.innerText = 'X';
+        clickedElement.innerText = 'O';
+      }
+    }
+    console.log("ran")
