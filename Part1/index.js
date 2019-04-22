f99999d88c652c8184c44d5df496be292e18757a
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-22 16:06:40 -0600

    twelfth

--- Part1/index.js
+++ Part1/index.js
@@ -7,9 +7,11 @@ function play(clickedId) {
     if (playerSpan.innerText === 'X') {
         playerSpan.innerText = 'O';
         clickedElement.innerText = 'X';
+        board[clickedId] = 'X';
       } else {
         playerSpan.innerText = 'X';
         clickedElement.innerText = 'O';
+        board[clickedId] = 'O';
       }
     
     console.log("board")
@@ -24,36 +26,92 @@ function play(clickedId) {
   const bottomCenter = board[7];
   const bottomRight = board[8];
 
-if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+  if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
     alert(`${topLeft} is the winner`);
     return;
   }
-if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+  if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
     alert(`${middleLeft} is the winner`);
     return;
-    }
-if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+  }
+  if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
     alert(`${bottomLeft} is the winner`);
     return;
-        }
-if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+  }
+  if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
     alert(`${topLeft} is the winner`);
     return;
-}
-if (topRight !== undefined && topRight === middleCenter && middleLeft === bottomLeft) {
-    alert(`${topRight} is the winner`);
-    return;
-    }
-if (topRight !== undefined && topRight === middleCenter && topRight === bottomRight) {
-    alert(`${topRight} is the winner`);
-    return;
-        }
-if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
-    alert(`${topLeft} is the winner`);
-    return;
-            }
-if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+  }
+  if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
     alert(`${topCenter} is the winner`);
     return;
-             }
+  }
+  if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
+    alert(`${topRight} is the winner`);
+    return;
+  }
+  if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+    alert(`${topLeft} is the winner`);
+    return;
+  }
+  if (bottomLeft !== undefined && bottomLeft === middleCenter && bottomLeft === topRight) {
+    alert(`${bottomLeft} is the winner`);
+    return;
+  }
+
+  let boardFull = true;
+  for (let i = 0; i <= 8; i++) {
+    if (board[i] === undefined) {
+      boardFull = false;
+    }
+  }
+  if (boardFull === true) {
+    alert("Cat's game, there is no winner");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+//     alert(`${topLeft} is the winner`);
+//     return;
+//   }
+// if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+//     alert(`${middleLeft} is the winner`);
+//     return;
+//     }
+// if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+//     alert(`${bottomLeft} is the winner`);
+//     return;
+//         }
+// if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+//     alert(`${topLeft} is the winner`);
+//     return;
+// }
+// if (topRight !== undefined && topRight === middleCenter && middleLeft === bottomLeft) {
+//     alert(`${topRight} is the winner`);
+//     return;
+//     }
+// if (topRight !== undefined && topRight === middleCenter && topRight === bottomRight) {
+//     alert(`${topRight} is the winner`);
+//     return;
+//         }
+// if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
+//     alert(`${topLeft} is the winner`);
+//     return;
+//             }
+// if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+//     alert(`${topCenter} is the winner`);
+//     return;
+//              }
         }
