e7329f1d10fafa325cd176e0a7c672f27b7eab10
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-22 17:25:39 -0600

    Fourth

--- Part2/index.js
+++ Part2/index.js
@@ -1,5 +1,7 @@
 const idInput = document.getElementById('idInput');
 const colorInput = document.getElementById('colorInput');
 
-console.log(idInput);
-console.log(colorInput);
+function setCard() {
+    const card = document.getElementById(idInput.value);
+    console.log(card);
+  }
